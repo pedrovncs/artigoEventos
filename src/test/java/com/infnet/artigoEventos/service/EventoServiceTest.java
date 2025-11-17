24431d9224e0f5745b0a510aab8e5e72df719e9d
pedro <pdrovncs@gmail.com>
2025-11-17 12:54:21 -0300

    teste falso para workflow

---
+++ src/test/java/com/infnet/artigoEventos/service/EventoServiceTest.java
@@ -0,0 +1,12 @@
+package com.infnet.artigoEventos.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventoServiceTest {
+
+    @Test
+    void getAllEventos() {
+    }
+}
