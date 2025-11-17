b5485bd27f1e05b42eb505fba62d0652755a78e2
pedro <pdrovncs@gmail.com>
2025-11-17 12:48:28 -0300

    add falha com cobertura baixa usando action

--- src/main/java/com/infnet/artigoEventos/controller/ApiController.java
+++ src/main/java/com/infnet/artigoEventos/controller/ApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*")
 public class ApiController {
-//
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
